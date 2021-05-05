3bac0766254b861d6e6e266ab52436caad7afd68
surya08-alt <77626372+surya08-alt@users.noreply.github.com>
2021-05-05 20:26:12 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,25 @@
+class SlingShot {
+    constructor(bodyA,pointB){
+    var options={
+    bodyA:bodyA,
+    pointB:pointB,
+    stiffness:0.4,
+    length:20
+    }
+    this.pointB=pointB
+     this.launcher=Constraint.create(options)
+     World.add(world,this.launcher)
+  }
+    fly(){
+      this.launcher.bodyA=null
+    }
+    attach(body){
+        this.launcher.bodyA=body
+    }
+     display(){
+     if(this.launcher.bodyA){
+     var pointA=this.launcher.bodyA.position  
+     var pointB=this.launcher.pointB
+      strokeWeight(3)
+     line(pointA.x,pointA.y,pointB.x,pointB.y)
+  }}}
